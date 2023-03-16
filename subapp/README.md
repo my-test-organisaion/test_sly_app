b46c57e3a9b404453f402dbc69727dea770dde5a
Nikolai Petukhov <petukhov.niko@gmail.com>
2023-03-16 10:06:49 -0300

    init

---
+++ subapp/README.md
@@ -0,0 +1,1 @@
+### TEST APP (SUB APP)
---
+++ subapp/src/main.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+import supervisely as sly
+from supervisely.app.widgets import Text, Container
+
+
+load_dotenv("local.env")
+load_dotenv(os.path.expanduser("~/supervisely.env"))
+
+layout = Container(widgets=[Text("Hello World"), Text("I'm Subapp")])
+
+app = sly.Application(layout=layout)

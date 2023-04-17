993a684696e183dca78422cbcef4675fffa79a92
Nikolai Petukhov <petukhov.niko@gmail.com>
2023-04-17 14:00:05 -0300

    test

--- src/main.py
+++ src/main.py
@@ -1,29 +1,36 @@
 import os
 from dotenv import load_dotenv
 import supervisely as sly
-from supervisely.app.widgets import Text, Container
+from supervisely.app.widgets import Button, Container, Button
 
 
 load_dotenv("local.env")
 load_dotenv(os.path.expanduser("~/supervisely.env"))
 
-arg1 = os.environ.get("arg1.value")
-arg2 = os.environ.get("arg2.value")
-arg3 = os.environ.get("arg3.value")
-arg4 = os.environ.get("arg4.checked")
-arg5 = os.environ.get("arg5.value")
-arg6 = os.environ.get("arg6.value")
+team_id = sly.env.team_id()
+
+api = sly.Api()
+
+stop_button = Button("stop")
+print_button = Button("print")
+
+buttons = []
+for i in range(50):
+    b = Button(f"button {i}")
+    b.loading = True
+    buttons.append(b)
 
 layout = Container(
-    widgets=[
-        Text("Hello World"),
-        Text(arg1),
-        Text(arg2),
-        Text(arg3),
-        Text(arg4),
-        Text(arg5),
-        Text(arg6),
-    ]
+    widgets=[stop_button, print_button, *buttons]
 )
-
 app = sly.Application(layout=layout)
+
+@stop_button.click
+def b_stop():
+    app.stop()
+    for b in buttons:
+        b.loading = False
+
+@print_button.click
+def b_print():
+    print("print clicked")

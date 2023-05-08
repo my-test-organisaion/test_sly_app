c84d8907d57b72767db85af9da1cffcc1cff4686
Nikolai Petukhov <petukhov.niko@gmail.com>
2023-05-08 12:50:25 -0300

    fix

--- release_all_subapps.sh
+++ release_all_subapps.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if (( ${#SUBAPP_PATHS[@]} == 0 )); then
-    supervisely release --release-version "${{ github.event.release.tag_name }}" --release-description "${{ github.event.release.name }}" --slug "${{ github.repository }}" -y
+    supervisely release --release-version "$RELEASE_VERSION" --release-description "$RELEASE_DESCRIPTION" --slug "$SLUG" -y
 else
     for i in "${SUBAPP_PATHS[@]}"
     do
-        if $i == "__ROOT_APP__"
+        if [[ "$i" == "__ROOT_APP__" ]]
         then
-            supervisely release --release-version "${{ github.event.release.tag_name }}" --release-description "${{ github.event.release.name }}" --slug "${{ github.repository }}" -y
+            supervisely release --release-version "$RELEASE_VERSION" --release-description "$RELEASE_DESCRIPTION" --slug "$SLUG" -y
         else
-            supervisely release --release-version "${{ github.event.release.tag_name }}" --release-description "${{ github.event.release.name }}" --slug "${{ github.repository }}" -y -a $i
+            supervisely release --release-version "$RELEASE_VERSION" --release-description "$RELEASE_DESCRIPTION" --slug "$SLUG" -y -a $i
         fi
     done
 fi
